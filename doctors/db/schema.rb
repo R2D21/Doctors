05a29bd67a19057486950c2e857da796d8f341ee
jjeleven11 <jjdraa@gmail.com>
2018-07-26 17:19:36 +0200

    create seed

--- doctors/db/schema.rb
+++ doctors/db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_115858) do
+ActiveRecord::Schema.define(version: 2018_07_26_144234) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "doctor_id"
+    t.integer "patient_id"
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
